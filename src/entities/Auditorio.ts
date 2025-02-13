1340b9f61dc4d82dbba88a3cdf351aff4309e709
lucas2106 <80073835+lucas2106@users.noreply.github.com>
2025-02-13 10:46:49 -0300

    auditorio mudanaças

--- src/entities/Auditorio.ts
+++ src/entities/Auditorio.ts
@@ -1,5 +1,28 @@
-class Auditorio {
-    
-}
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
-export default Auditorio;
+@Entity("auditoriums")
+export class Auditorium {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    capacity: number;
+
+    @Column()
+    location: string;
+
+    @Column({ default: false })
+    has_projector: boolean;
+
+    @Column({ default: false })
+    has_sound_system: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
